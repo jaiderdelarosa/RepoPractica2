c3bd28b248ce1a3ef4ea90543acd72dbe5d0071d
Fer912909 <fernandopinto2689@gmail.com>
2025-10-27 20:30:39 -0500

    feat: Agregar clase Estudiante

---
+++ EjercicioHerencia/src/Estudiante.java
@@ -0,0 +1,21 @@
+public class Estudiante {
+    String nombre;
+    String curso;
+    public Estudiante(String nombre, String curso) {
+        this.nombre = nombre;
+        this.curso = curso;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+     
+}
